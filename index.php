264e46387bee370411e2e66fe88bdee54378af77
Evan Wheeler <effyn@gmx.com>
2019-06-11 02:50:25 -0400

    Error logic for form 1

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ error_reporting(E_ALL);
 
 //require the autoload file autoload.php
 require_once('vendor/autoload.php');
+require_once('model/validation-functions.php');
 
 //Create an instance of the Base class/ instantiate Fat-Free
 $f3 = Base::instance();
@@ -47,9 +48,20 @@ $f3->route('GET /@animal', function($f3, $params) {
         }
 });
 
-$f3->route('GET /order', function() {
-    $view = new Template();
-    echo $view->render('views/form1.html');
+$f3->route('GET|POST /order',function($f3) {
+    $_SESSION = array();
+
+    if (isset($_POST['animal'])) {
+        if (validString($_POST['animal'])) {
+            $_SESSION['animal'] = $_POST['animal'];
+            $f3->reroute('/order2');
+        } else {
+            $f3->set("errors['animal']", "Please enter an animal.");
+        }
+    }
+
+    $template = new Template();
+    echo $template->render('views/form1.html');
 });
 
 $f3->route('POST /order2', function() {
