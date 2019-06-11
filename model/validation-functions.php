170bff25ed4b1af9d589cc2722c7662007eb483f
Zane Stearman <zstearman@mail.greenriver.edu>
2019-06-11 02:40:20 -0400

    add string validation

--- model/validation-functions.php
+++ model/validation-functions.php
@@ -5,3 +5,7 @@ function validColor($color)
     global $f3;
     return in_array($color, $f3->get('colors'));
 }
+
+function validString($string) {
+    return !empty($string) && ctype_alpha($string);
+}
