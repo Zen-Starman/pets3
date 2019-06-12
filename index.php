ac29f2ee7a7bf45a7cfff46a787b798e1f1e1c8c
Zane Stearman <zstearman@mail.greenriver.edu>
2019-06-12 19:21:36 -0400

    Add check statements and validation

--- index.php
+++ index.php
@@ -23,30 +23,30 @@ $f3->route('GET /', function()
     echo $view->render('views/home.html');
 });
 
-//Define a route with a parameter
-$f3->route('GET /@animal', function($f3, $params) {
-    $animal = $params['animal'];
-
-        switch ($animal) {
-            case 'dog':
-                echo "<h3>Woof!</h3>";
-                break;
-            case 'cat':
-                echo "<h3>Meow</h3>";
-                break;
-            case 'pig':
-                echo "<h3>Oink</h3>";
-                break;
-            case 'bear':
-                echo "<h3>Grrr</h3>";
-                break;
-            case 'bird':
-                echo "<h3>Hello</h3>";
-                break;
-            default:
-                $f3->error(404);
-        }
-});
+////Define a route with a parameter
+//$f3->route('GET /@animal', function($f3, $params) {
+//    $animal = $params['animal'];
+//
+//        switch ($animal) {
+//            case 'dog':
+//                echo "<h3>Woof!</h3>";
+//                break;
+//            case 'cat':
+//                echo "<h3>Meow</h3>";
+//                break;
+//            case 'pig':
+//                echo "<h3>Oink</h3>";
+//                break;
+//            case 'bear':
+//                echo "<h3>Grrr</h3>";
+//                break;
+//            case 'bird':
+//                echo "<h3>Hello</h3>";
+//                break;
+//            default:
+//                $f3->error(404);
+//        }
+//});
 
 $f3->route('GET|POST /order',function($f3) {
     $_SESSION = array();
@@ -64,13 +64,22 @@ $f3->route('GET|POST /order',function($f3) {
     echo $template->render('views/form1.html');
 });
 
-$f3->route('POST /order2', function() {
-    $_SESSION['animal'] = $_POST['animal'];
+$f3->route('GET|POST /order2', function($f3) {
+
+    if (isset($_POST['color'])) {
+        if (validColor($_POST['color'])) {
+            $_SESSION['color'] = $_POST['color'];
+            $f3->reroute('/results');
+        } else {
+            $f3->set("errors['color']", "Please choose a color.");
+        }
+    }
+
     $template = new Template();
     echo $template->render('views/form2.html');
 });
 
-$f3->route('POST /results', function() {
+$f3->route('GET|POST /results', function() {
     $_SESSION['color'] = $_POST['color'];
     $template = new Template();
     echo $template->render('views/results.html');
